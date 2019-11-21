397083bc20762dc1bc4b1fd383c02120ed6cfb91
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-21 08:40:45 -0500

    card array

--- ch4/script.js
+++ ch4/script.js
@@ -6,3 +6,11 @@ console.log(value);
 value = 10 / 0;
 
 console.log(value);
+
+let cards = [ "Ace", "King", "Queen", "Jack"];
+
+console.log( cards[0] );
+console.log( cards[1] );
+console.log( cards[2] );
+console.log( cards[3] );
+console.log( cards[4] );
