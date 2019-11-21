b87aad2f028bb70c50f831ce188eabd49e01c08c
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-21 10:49:06 -0500

    added array of cards

--- blackjack/script.js
+++ blackjack/script.js
@@ -2,12 +2,20 @@
 // blackjack
 //
 
-let deck = [
-    "Ace of Spades",
-    "Two of Spades",
-    "Three of Spades"
-];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
 
+let deck = [];
+
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+    }
+}
+
+for (let i = 0; i<deck.length; i++) {
+    console.log ( deck[i]);
+}
 let playerCards = [ deck[0], deck[2] ];
 
 console.log("Welcome to Blackjack");
