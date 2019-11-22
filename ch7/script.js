c3618ccd3c02fdd04d2a11778b2e3ccadf56b9d2
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-22 09:25:34 -0500

    create card object

---
+++ ch7/script.js
@@ -0,0 +1,18 @@
+function changeCard(card) {
+    card.suit = "Clubs";
+}
+
+let cards = [
+    {
+    suit : "Hearts",
+    value : "Queen"
+    },
+    {
+        suit : "Clubs",
+        value : "King"
+    }
+    
+];
+
+console.log (cards[1].suit);
+console.log (cards[1].value);
