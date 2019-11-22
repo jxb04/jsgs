06385930664cd3fbb7b40323ef5cd0ec85c04d54
Joel Bernasky <joel_bernasky@hotmail.com>
2019-11-22 08:50:19 -0500

    add functions to blackjack

--- blackjack/script.js
+++ blackjack/script.js
@@ -5,21 +5,29 @@
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
 
-let deck = [];
-
-for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+function createDeck() {
+    let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+        }
     }
+    return deck;
 }
 
-for (let i = 0; i<deck.length; i++) {
-    console.log ( deck[i]);
+let deck = createDeck();
+
+function getNextCard() {
+    return deck.shift();
 }
-let playerCards = [ deck[0], deck[2] ];
+
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+let playerCards = [getNextCard(), getNextCard()];
 
 console.log("Welcome to Blackjack");
 
 console.log("Your are dealt: ");
-console.log(" " + playerCards[0] );
-console.log(" " + playerCards[1] );
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
